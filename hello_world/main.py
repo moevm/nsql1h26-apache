cf0d8c09f392268eabea7333824e743321e3cd35
Волков Александр <sanya005volkov@mail.ru>
2026-02-16 21:07:01 +0300

    Add MongoDB hello_world insert/read script

---
+++ hello_world/main.py
@@ -0,0 +1,27 @@
+from pymongo import MongoClient
+from datetime import datetime
+
+
+def main():
+    client = MongoClient("mongodb://localhost:27017/", serverSelectionTimeoutMS=2000)
+
+    db = client["hello_db"]
+    coll = db["hello_collection"]
+
+    data_to_insert = {
+        "message": "Первый запуск",
+        "created_at": datetime.now()
+    }
+
+    try:
+        insert_id = coll.insert_one(data_to_insert).inserted_id
+        print(f"Вставка успешно выполнена! ID={insert_id}")
+
+        found = coll.find_one({"_id": insert_id})
+        print(f"Данные из базы: {found}")
+    except Exception as e:
+        print(f"Ошибка!\n {e}")
+
+
+if __name__ == "__main__":
+    main()
